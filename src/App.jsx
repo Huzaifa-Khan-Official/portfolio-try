eba4f24a263c42478622c1dd07b38e5859c5577a
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-03-14 17:12:49 +0500

    styling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <ScrollSpy>
-        <div id="first">
+        <div id="home">
           <h2>First Para</h2>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, cumque, amet doloremque ab possimus libero excepturi inventore obcaecati accusantium animi debitis sint eligendi rem, dignissimos officiis aliquam minima! Necessitatibus, adipisci!
           Vel quae officia quam. Vero eum deserunt, soluta dolores odio dicta deleniti iure eveniet a adipisci facere! Dolores itaque, deleniti voluptates unde quam exercitationem, illum neque, doloremque nam nulla repellat!
@@ -18,7 +18,7 @@ export default function App() {
           Sapiente nobis, nisi porro totam eius numquam harum, sequi eos consequuntur vero obcaecati omnis culpa animi repellendus a recusandae saepe dicta? Ut inventore, quia laborum optio ratione ab distinctio dicta.
           Dolores odio at quas, a iusto expedita, animi harum obcaecati porro, repellendus officia nulla. Accusamus quaerat similique deserunt accusantium a ipsum aperiam. Quam aspernatur possimus ratione, neque quod tempora reprehenderit.
         </div>
-        <div id="second">
+        <div id="service">
           <h2>Second Para</h2>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, cumque, amet doloremque ab possimus libero excepturi inventore obcaecati accusantium animi debitis sint eligendi rem, dignissimos officiis aliquam minima! Necessitatibus, adipisci!
           Vel quae officia quam. Vero eum deserunt, soluta dolores odio dicta deleniti iure eveniet a adipisci facere! Dolores itaque, deleniti voluptates unde quam exercitationem, illum neque, doloremque nam nulla repellat!
@@ -31,7 +31,7 @@ export default function App() {
           Sapiente nobis, nisi porro totam eius numquam harum, sequi eos consequuntur vero obcaecati omnis culpa animi repellendus a recusandae saepe dicta? Ut inventore, quia laborum optio ratione ab distinctio dicta.
           Dolores odio at quas, a iusto expedita, animi harum obcaecati porro, repellendus officia nulla. Accusamus quaerat similique deserunt accusantium a ipsum aperiam. Quam aspernatur possimus ratione, neque quod tempora reprehenderit.
         </div>
-        <div id="third">
+        <div id="portfolio">
           <h2>Third Para</h2>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, cumque, amet doloremque ab possimus libero excepturi inventore obcaecati accusantium animi debitis sint eligendi rem, dignissimos officiis aliquam minima! Necessitatibus, adipisci!
           Vel quae officia quam. Vero eum deserunt, soluta dolores odio dicta deleniti iure eveniet a adipisci facere! Dolores itaque, deleniti voluptates unde quam exercitationem, illum neque, doloremque nam nulla repellat!
@@ -44,7 +44,35 @@ export default function App() {
           Sapiente nobis, nisi porro totam eius numquam harum, sequi eos consequuntur vero obcaecati omnis culpa animi repellendus a recusandae saepe dicta? Ut inventore, quia laborum optio ratione ab distinctio dicta.
           Dolores odio at quas, a iusto expedita, animi harum obcaecati porro, repellendus officia nulla. Accusamus quaerat similique deserunt accusantium a ipsum aperiam. Quam aspernatur possimus ratione, neque quod tempora reprehenderit.
         </div>
-        <div id="fourth">
+        <div id="skill">
+          <h2>Fourth Para</h2>
+
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, cumque, amet doloremque ab possimus libero excepturi inventore obcaecati accusantium animi debitis sint eligendi rem, dignissimos officiis aliquam minima! Necessitatibus, adipisci!
+          Vel quae officia quam. Vero eum deserunt, soluta dolores odio dicta deleniti iure eveniet a adipisci facere! Dolores itaque, deleniti voluptates unde quam exercitationem, illum neque, doloremque nam nulla repellat!
+          Consectetur suscipit ex dolor tempore eligendi nisi modi sunt consequuntur ut voluptatibus nobis aspernatur ducimus quaerat inventore obcaecati harum asperiores porro neque labore, iusto deleniti nulla, fuga eum saepe! Placeat?
+          Eveniet consequuntur recusandae odio quo ab corporis, et delectus laudantium voluptates labore, nam facere quidem minus quod commodi ipsam sint excepturi dignissimos similique aspernatur nostrum deserunt repudiandae repellat porro? Porro!
+          Aliquam officiis neque, veniam libero temporibus facilis id officia minima corporis aut itaque impedit soluta veritatis obcaecati, modi nesciunt inventore! Dolores corrupti, quasi ex voluptas hic autem dolorum architecto distinctio?
+          Inventore perspiciatis vitae modi blanditiis corrupti ullam eum suscipit nesciunt vero nam quidem nisi, error eaque obcaecati facilis est ipsum ipsa voluptas in cum? Dolores hic esse totam voluptatibus aliquam.
+          Nostrum nihil cupiditate magni minima. Quis eaque nemo perferendis ipsam non harum consectetur distinctio repellendus provident sequi, dolore corrupti nesciunt! Architecto sed esse distinctio illum nemo est perspiciatis quod. A.
+          Nostrum, fugiat. Nam facere a similique possimus, porro aperiam ratione fuga et illo modi dolores dolore repellat consequuntur suscipit voluptatibus earum excepturi sit eius. Harum autem porro soluta blanditiis corrupti.
+          Sapiente nobis, nisi porro totam eius numquam harum, sequi eos consequuntur vero obcaecati omnis culpa animi repellendus a recusandae saepe dicta? Ut inventore, quia laborum optio ratione ab distinctio dicta.
+          Dolores odio at quas, a iusto expedita, animi harum obcaecati porro, repellendus officia nulla. Accusamus quaerat similique deserunt accusantium a ipsum aperiam. Quam aspernatur possimus ratione, neque quod tempora reprehenderit.
+        </div>
+        <div id="testimonial">
+          <h2>Fourth Para</h2>
+
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, cumque, amet doloremque ab possimus libero excepturi inventore obcaecati accusantium animi debitis sint eligendi rem, dignissimos officiis aliquam minima! Necessitatibus, adipisci!
+          Vel quae officia quam. Vero eum deserunt, soluta dolores odio dicta deleniti iure eveniet a adipisci facere! Dolores itaque, deleniti voluptates unde quam exercitationem, illum neque, doloremque nam nulla repellat!
+          Consectetur suscipit ex dolor tempore eligendi nisi modi sunt consequuntur ut voluptatibus nobis aspernatur ducimus quaerat inventore obcaecati harum asperiores porro neque labore, iusto deleniti nulla, fuga eum saepe! Placeat?
+          Eveniet consequuntur recusandae odio quo ab corporis, et delectus laudantium voluptates labore, nam facere quidem minus quod commodi ipsam sint excepturi dignissimos similique aspernatur nostrum deserunt repudiandae repellat porro? Porro!
+          Aliquam officiis neque, veniam libero temporibus facilis id officia minima corporis aut itaque impedit soluta veritatis obcaecati, modi nesciunt inventore! Dolores corrupti, quasi ex voluptas hic autem dolorum architecto distinctio?
+          Inventore perspiciatis vitae modi blanditiis corrupti ullam eum suscipit nesciunt vero nam quidem nisi, error eaque obcaecati facilis est ipsum ipsa voluptas in cum? Dolores hic esse totam voluptatibus aliquam.
+          Nostrum nihil cupiditate magni minima. Quis eaque nemo perferendis ipsam non harum consectetur distinctio repellendus provident sequi, dolore corrupti nesciunt! Architecto sed esse distinctio illum nemo est perspiciatis quod. A.
+          Nostrum, fugiat. Nam facere a similique possimus, porro aperiam ratione fuga et illo modi dolores dolore repellat consequuntur suscipit voluptatibus earum excepturi sit eius. Harum autem porro soluta blanditiis corrupti.
+          Sapiente nobis, nisi porro totam eius numquam harum, sequi eos consequuntur vero obcaecati omnis culpa animi repellendus a recusandae saepe dicta? Ut inventore, quia laborum optio ratione ab distinctio dicta.
+          Dolores odio at quas, a iusto expedita, animi harum obcaecati porro, repellendus officia nulla. Accusamus quaerat similique deserunt accusantium a ipsum aperiam. Quam aspernatur possimus ratione, neque quod tempora reprehenderit.
+        </div>
+        <div id="contact">
           <h2>Fourth Para</h2>
 
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, cumque, amet doloremque ab possimus libero excepturi inventore obcaecati accusantium animi debitis sint eligendi rem, dignissimos officiis aliquam minima! Necessitatibus, adipisci!
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,64 @@
 import React from 'react'
-import { Link, Navigate } from 'react-router-dom'
 
 export default function Navbar({ children }) {
     return (
         <div>
-            <nav id="navbar-example2" className="navbar bg-body-tertiary px-3 mb-3 sticky-top">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <ul className="nav nav-pills">
-                    <li className="nav-item">
-                        <a href="#first" className="nav-link" data-to-scrollspy-id="first">First</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#second" className="nav-link" data-to-scrollspy-id="second">Second</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#third" className="nav-link" data-to-scrollspy-id="third">Third</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#fourth" className="nav-link" data-to-scrollspy-id="fourth">Fourth</a>
-                    </li>
-                </ul>
+            <nav className="navbar navbar-expand-md bg-body-tertiary sticky-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbarToggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a href="#home" className="nav-link" data-to-scrollspy-id="home">
+                                    home
+                                    <span id='line'></span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#service" className="nav-link" data-to-scrollspy-id="service">
+                                    service
+                                    <span id='line'></span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#portfolio" className="nav-link" data-to-scrollspy-id="portfolio">
+                                    portfolio
+                                    <span id='line'></span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#skill" className="nav-link" data-to-scrollspy-id="skill">
+                                    skill
+                                    <span id='line'></span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#testimonial" className="nav-link" data-to-scrollspy-id="testimonial">
+                                    testimonial
+                                    <span id='line'></span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#contact" className="nav-link" data-to-scrollspy-id="contact">
+                                    contact
+                                    <span id='line'></span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
             </nav>
+
+
+
+
+
+
+
 
             {children}
         </div>
